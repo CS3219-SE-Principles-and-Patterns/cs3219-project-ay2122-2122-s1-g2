12660010a7a48fea694730b7b2e9f8f023638910
Lim Wei Quan, Ernest <ernestlim8@gmail.com>
2021-09-23 21:44:22 +0800

    Integrate JWT token and login with frontend

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,19 +1,39 @@
 import "./LandingPage.css";
+import { useForm } from "react-hook-form";
+import { loginUser } from "../infra/auth";
+import { login } from "../utils/auth/auth";
 
 const LandingPage = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    // do something
+    let response = await loginUser(data).catch((e) => {
+      setError("password", { message: e.response.data });
+    });
+
+    login(response.data.accessToken);
+  };
+
   return (
     <div className="container">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
         <h1>Login</h1>
         <div className="login-input">
           <label>Username:</label>
-          <input />
+          <input {...register("username")} />
         </div>
         <div className="login-input">
           <label>Password:</label>
-          <input />
+          <input {...register("password")} />
+          {errors.password && <p>{errors.password.message}</p>}
         </div>
-        <button>Log In</button>
+        <button type="submit">Log In</button>
       </form>
     </div>
   );
