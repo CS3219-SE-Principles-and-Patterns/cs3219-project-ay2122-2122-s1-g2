ae6377aeed051cd2676f08e425b050aa80c8c005
KendrewC <kendrewchanys@gmail.com>
2021-09-27 14:47:11 +0800

    Fix merge conflicts

--- backend/login/routes/loginRoutes.js
+++ backend/login/routes/loginRoutes.js
@@ -5,16 +5,7 @@ const DatabaseManager = require("../database/loginDatabase.js");
 
 router.get("/", DatabaseManager.getAll); 
 
-<<<<<<< HEAD
 router.get("/:username", DatabaseManager.get);
-=======
-const authTokenMW = (req, res, next) => {
-	const aHeader = req.headers['authorization'];
-	const aToken = aHeader && aHeader.split(' ')[1];
-	if (aToken == null) {
-		return res.status(401).send("Authentication token required");
-	}
->>>>>>> master
 
 router.post("/login", DatabaseManager.login);
 
