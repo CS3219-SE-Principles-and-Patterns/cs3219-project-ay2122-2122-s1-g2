81a37d5018c5cbc8e7a701481ed82276827b1f85
KendrewC <kendrewchanys@gmail.com>
2021-10-02 22:18:15 +0800

    Update Game and Matchmaking microservice

--- backend/matchmaking/database/matchmakingDatabase.js
+++ backend/matchmaking/database/matchmakingDatabase.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const db = process.env.MONGO_URI;
 // Connect to MongoDB
@@ -20,6 +21,22 @@ connection.once("open", (err) => {
 const Game = require("../models/game.js");
 
 const DatabaseManager = {
+    authTokenMW: (req, res, next) => {
+		const aHeader = req.headers['authorization'];
+		const aToken = aHeader && aHeader.split(' ')[1];
+		if (aToken == null) {
+			return res.status(401).send("Authentication token required");
+		}
+	
+		jwt.verify(aToken, ACCESS_SECRET, (err, user) => {
+			if (err) {
+				return res.status(403).send("Access Denied: Token is no longer valid");
+			}
+			req.user = user;
+			next();
+		})
+	},
+    
 }
 
 module.exports = DatabaseManager;
