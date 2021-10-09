29fa8f2802d2bb87b14b514e3b700b7463097d88
Ebolaeater <53635104+Ebolaeater@users.noreply.github.com>
2021-10-09 17:05:37 +0800

    Add basic code for game frontend

--- frontend/src/pages/game/GamePage.tsx
+++ frontend/src/pages/game/GamePage.tsx
@@ -1,9 +1,77 @@
 import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Stack,
+  Grid
+} from "@mui/material";
+import { GameUser } from "../../domain/gameUser";
+import { GameController } from "../../controller/GameController";
 
-const GamePage = () => {
+const GamePage = (props: any) => {
+  const [ratings, setRatings] = useState<number[]>([]);
+  const [languages, setLanguages] = useState<string[]>([]);
+  const [results, setResults] = useState<boolean[]>([]);
+  const [languageHistory, setLanguageHistory] = useState<string[]>([]);
+  
+  useEffect(() => {
+    const fetchHistory = async () => {
+      try {
+        const history: GameUser = await GameController.getUserHistory(props.username);
+        setLanguages(history.languages);
+        setResults(history.resultHistory);
+        setLanguageHistory(history.languageHistory);
+        setRatings(history.ratings);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchHistory();
+  }, []);
+
   return (
     <Box sx={{ flexGrow: 1 }} textAlign="center">
       <h1>Game</h1>
+      <Grid container justifyContent="center">
+        <Box component="span">
+          <Stack>
+            <List component="nav">
+              {languages.map((language) => {
+                <ListItem>
+                  <ListItemText inset primary={language}/>
+                </ListItem>
+              })}
+            </List>
+            <List component="nav">
+              {ratings.map((rating) => {
+                <ListItem>
+                  <ListItemText inset primary={rating}/>
+                </ListItem>
+              })}
+            </List>
+          </Stack>
+        </Box>
+        <Box component="span">
+          <Stack>
+            <List component="nav">
+              {languageHistory.map((language) => {
+                <ListItem>
+                  <ListItemText inset primary={language}/>
+                </ListItem>
+              })}
+            </List>
+            <List component="nav">
+              {results.map((result) => {
+                <ListItem>
+                  <ListItemText inset primary={result}/>
+                </ListItem>
+              })}
+            </List>
+          </Stack>
+        </Box>
+      </Grid>
     </Box>
   );
 };
