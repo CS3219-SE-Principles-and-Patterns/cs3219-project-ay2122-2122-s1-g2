e12178f54accbfb400d4eaafddc18474428043ff
Lim Wei Quan, Ernest <ernestlim8@gmail.com>
2021-10-10 22:31:58 +0800

    Fix css for login and register pages

--- frontend/src/components/login/Register.jsx
+++ frontend/src/components/login/Register.jsx
@@ -3,6 +3,9 @@ import { useForm } from "react-hook-form";
 import { registerUser } from "../../infra/auth";
 import { landingEnum } from "../../utils/constants/enums";
 import CreateProfilePage from "../../pages/profile/CreateProfilePage";
+import { FormControl, Grid, Typography } from "@mui/material";
+import { CssButton, CssTextField } from "../../pages/common/Components";
+import { Box } from "@mui/system";
 
 const Register = ({ setLandingStatus }) => {
   const [registered, setRegistered] = useState(false);
@@ -28,53 +31,79 @@ const Register = ({ setLandingStatus }) => {
       setPage(1);
     }
   };
-  return page === 0 ? (
-    <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-      <h1>Register</h1>
-      <div className="login-input">
-        <label>Username:</label>
-        <input {...register("username", { required: true })} />
-        {errors.username?.type === "required" && (
-          <p className="error-message">Username is required!</p>
-        )}
-        {errors.username && (
-          <p className="error-message">{errors.username.message}</p>
-        )}
-      </div>
-      <div className="login-input">
-        <label>Password:</label>
-        <input {...register("password", { required: true })} />
-        {errors.password?.type === "required" && (
-          <p className="error-message">Password is required!</p>
-        )}
-      </div>
-      <div className="login-input">
-        <label>Re-enter Password:</label>
-        <input
-          {...register("validatePassword", {
-            validate: (value) =>
-              value === password.current || "The passwords do not match.",
-          })}
-        />
-        {errors.validatePassword && (
-          <p className="error-message">{errors.validatePassword.message}</p>
-        )}
-      </div>
-      {registered && (
-        <p className="success-message">Successfully registered!</p>
+  return (
+    <Grid container sx={{ width: "80vw" }}>
+      <Grid container sm={6}></Grid>
+      {page === 0 ? (
+        <Grid container sm={6}>
+          <Grid item xs={5}></Grid>
+          <Grid item xs={7}>
+            <Typography>
+              Already have an Account?{" "}
+              <b onClick={() => setLandingStatus(landingEnum.LOGIN)}>Login</b>
+            </Typography>
+          </Grid>
+          <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+            <Typography variant="h4">Sign Up</Typography>
+            <FormControl>
+              <CssTextField
+                label="Username"
+                {...register("username", { required: true })}
+              />
+              {errors.username?.type === "required" && (
+                <p className="error-message">Username is required!</p>
+              )}
+              {errors.username && (
+                <p className="error-message">{errors.username.message}</p>
+              )}
+            </FormControl>
+            <FormControl>
+              <CssTextField
+                label="Password"
+                {...register("password", { required: true })}
+              />
+              {errors.password?.type === "required" && (
+                <p className="error-message">Password is required!</p>
+              )}
+            </FormControl>
+            <FormControl>
+              <CssTextField
+                label="Re-enter Password"
+                {...register("validatePassword", {
+                  validate: (value) =>
+                    value === password.current || "The passwords do not match.",
+                })}
+              />
+              {errors.validatePassword && (
+                <p className="error-message">
+                  {errors.validatePassword.message}
+                </p>
+              )}
+            </FormControl>
+            {registered && (
+              <p className="success-message">Successfully registered!</p>
+            )}
+            <Box flex textAlign="center">
+              <CssButton
+                variant="outlined"
+                type="submit"
+                justifySelf="center"
+                sx={{ width: "50%" }}
+              >
+                Register
+              </CssButton>
+            </Box>
+          </form>
+        </Grid>
+      ) : (
+        <Grid container sm={6}>
+          <CreateProfilePage
+            username={getValues("username")}
+            setLandingStatus={setLandingStatus}
+          />
+        </Grid>
       )}
-      <div className="login-buttons">
-        <button type="submit">Register</button>
-        <button onClick={() => setLandingStatus(landingEnum.LOGIN)}>
-          Back to login
-        </button>
-      </div>
-    </form>
-  ) : (
-    <CreateProfilePage
-      username={getValues("username")}
-      setLandingStatus={setLandingStatus}
-    />
+    </Grid>
   );
 };
 
