c1fd8be086d7a7ebb90f96aa0c1722d5ff584342
Lim Wei Quan, Ernest <ernestlim8@gmail.com>
2021-10-31 14:19:53 +0800

    a

--- backend/login/index.js
+++ backend/login/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 const loginRoutes = require("./routes/loginRoutes.js");
 
-app.use("/api/login", loginRoutes);
+app.use("/", loginRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
