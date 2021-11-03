35e769eca5fb6f644e17564ea03a7e006361cbbd
Lim Wei Quan, Ernest <ernestlim8@gmail.com>
2021-11-03 18:51:10 +0800

    expose flashcard service

--- backend/login/index.js
+++ backend/login/index.js
@@ -10,15 +10,6 @@ const loginRoutes = require("./routes/loginRoutes.js");
 
 app.use("/api/login", loginRoutes);
 
-app.get("/healthz", (req, res) => {
-  res.send("ok");
-  console.log("ok outside");
-});
-
-app.get("/", (req, res) => {
-  res.send("success outside");
-  console.log("success outside");
-});
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server up and running on port ${port}!`);
